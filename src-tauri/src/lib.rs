55a1617cd822977882b953e73ef1c5141e5a8304
hortensia <824466875@qq.com>
2024-12-16 21:11:49 +0800

    add upload

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,11 +1,13 @@
 use std::sync::LazyLock;
 
-use tokio::sync::RwLock;
+use tauri_plugin_dialog::{DialogExt, FilePath};
+use tokio::{fs::File, sync::RwLock};
 
 use anyhow::Result;
 use serde::{Deserialize, Serialize};
 use tauri::Manager;
-use tauri_plugin_http::reqwest;
+use tauri_plugin_http::reqwest::{self, Body};
+use tokio_util::codec::{BytesCodec, FramedRead};
 
 pub enum ServeMode {
     LOCAL,
@@ -37,14 +39,16 @@ const TALK_ENDPOINT: &str = "/talk";
 
 const SESSION_ENDPOINT: &str = "/session";
 
+const UPLOAD_ENDPOINT: &str = "/upload";
+
 macro_rules! build_endpoint {
     ($endpoint: ident) => {
         LazyLock::new(|| format!("{}{}", REMOTE_ADDR, $endpoint))
     };
 }
 
-fn create_http_client() -> reqwest::Client {
-    reqwest::Client::builder().no_proxy().build().unwrap()
+fn create_http_client() -> Result<reqwest::Client> {
+    Ok(reqwest::Client::builder().no_proxy().build()?)
 }
 
 #[derive(Serialize)]
@@ -79,35 +83,91 @@ struct Session {
     session: usize,
 }
 
-fn open_session() -> usize {
+fn open_session() -> Result<usize> {
     static ENDPOINT: LazyLock<String> = build_endpoint!(SESSION_ENDPOINT);
     let json: Session = reqwest::blocking::Client::builder()
         .no_proxy()
-        .build()
-        .unwrap()
+        .build()?
         .get(&*ENDPOINT)
+        .send()?
+        .json()?;
+    Ok(json.session)
+}
+
+#[tauri::command]
+async fn pick_file(
+    app_handle: tauri::AppHandle,
+    state: tauri::State<'_, RwLock<ServeMode>>,
+) -> Result<(), ()> {
+    let file_path = pick_file_async(app_handle).await.ok_or(())?;
+    match &*state.read().await {
+        ServeMode::LOCAL => upload_local(),
+        ServeMode::REMOTE { http_client, .. } => {
+            upload_file(file_path, &http_client).await.unwrap()
+        }
+    };
+    Ok(())
+}
+
+fn upload_local() {
+    println!("upload later");
+}
+
+async fn pick_file_async(app_handle: tauri::AppHandle) -> Option<FilePath> {
+    tokio::task::spawn_blocking(move || app_handle.dialog().file().blocking_pick_file())
+        .await
+        .ok()
+        .unwrap_or_default()
+}
+
+fn file_to_body(file: File) -> Body {
+    let stream = FramedRead::new(file, BytesCodec::new());
+    let body = Body::wrap_stream(stream);
+    body
+}
+
+async fn upload_file(file_path: FilePath, client: &reqwest::Client) -> Result<()> {
+    static ENDPOINT: LazyLock<String> = build_endpoint!(UPLOAD_ENDPOINT);
+    let file = File::open(file_path.into_path().unwrap()).await?;
+    let res = client
+        .post(&*ENDPOINT)
+        .body(file_to_body(file))
         .send()
-        .unwrap()
-        .json()
-        .unwrap();
-    json.session
+        .await?;
+    println!("{:?}", res);
+    Ok(())
+}
+
+fn build_remote_app() -> Result<ServeMode> {
+    let http_client = create_http_client()?;
+    let session = open_session()?;
+    Ok(ServeMode::REMOTE {
+        http_client,
+        session,
+    })
+}
+
+fn build_app(app: &mut tauri::App) -> Result<(), Box<dyn std::error::Error>> {
+    let serve_mode = build_remote_app()
+        .map_err(|err| {
+            println!(
+                "connect to online host failed, fallback to local:\n {}",
+                err
+            )
+        })
+        .unwrap_or(ServeMode::LOCAL);
+    app.manage(RwLock::new(serve_mode));
+    Ok(())
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_shell::init())
-        .setup(|app| {
-            let http_client = create_http_client();
-            let session = open_session();
-            app.manage(RwLock::new(ServeMode::REMOTE {
-                http_client,
-                session,
-            }));
-            Ok(())
-        })
-        .invoke_handler(tauri::generate_handler![chat])
+        .setup(build_app)
+        .invoke_handler(tauri::generate_handler![chat, pick_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
